15eb3ddf9a7dc51972d480a61faada17c11e77ff
Leonardo <srleon19@hotmail.com>
2021-06-22 15:03:24 -0300

    NLW6 - React - Aula 1

---
+++ letmeask/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/Button'
+
+function App() {
+  return (
+    <div>
+    <Button text="Botão 1"/>
+    <Button text="Botão 2"/>
+    <Button text="Botão 3"/>
+    <Button text="Botão 4"/>
+    </div>
+  );
+}
+
+export default App;
